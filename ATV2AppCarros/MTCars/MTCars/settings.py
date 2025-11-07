d2dbc86a3163c8b85bc7ebe7d3824324d5b276a3
pgferrante <pedrogabrielbo2@gmail.com>
2025-11-07 13:09:51 +0000

    corrigido settings.py

--- ATV2AppCarros/MTCars/MTCars/settings.py
+++ ATV2AppCarros/MTCars/MTCars/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOW_ALL_ORIGINS = True
+CSRF_TRUSTED_ORIGINS = [
+    'https://localhost:8000', 
+    'http://localhost:8000',
+]
